8745c0e107aaf3aa4007bc9c8279c3b0ce3c6ba4
knmskln <lena.kaminskaya123@gmail.com>
2023-10-11 02:31:28 +0300

    feat: complete task

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrderService(ApplicationDbContext dbContext)
+    {
+        this._dbContext = dbContext;
+    }
+
+    public async Task<Order> GetOrder()
+    {
+        var order = await _dbContext.Orders
+            .OrderByDescending(o => o.Price * o.Quantity)
+            .FirstOrDefaultAsync();
+
+        return order;
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        var orders = await _dbContext.Orders
+            .Where(o => o.Quantity > 10)
+            .ToListAsync();
+
+        return orders;
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<User?> GetUser()
+    {
+        var userWithMostOrders = await _dbContext.Users
+            .Include(u => u.Orders)
+            .OrderByDescending(u => u.Orders.Count)
+            .FirstOrDefaultAsync();
+
+        return userWithMostOrders;
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        var inactiveUsers = await _dbContext.Users
+            .Where(u => u.Status == UserStatus.Inactive)
+            .ToListAsync();
+
+        return inactiveUsers;
+    }
+}
